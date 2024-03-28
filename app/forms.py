9079d80356c956ec40ddc85842edb04e9839d684
askmydog <stephen.koplin@gmail.com>
2024-03-27 21:23:07 -0400

    main

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import TextAreaField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
+from app import db
+from app.models import Notes
 
-class Note(FlaskForm):
+class NoteForm(FlaskForm):
     note = TextAreaField('Note', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class RiskForm(FlaskForm):
+    age = IntegerField('Age', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
 from flask import render_template, request, redirect, url_for,flash
 from app import app, db
 import sqlalchemy as sa
-from app.forms import Note
+from app.forms import NoteForm, RiskForm
 from app.models import Notes
 from functions import age_parsing
 
 @app.route('/', methods=['GET', 'POST'])
 def NoteInput():
-    form = Note()
+    form = NoteForm()
     if form.validate_on_submit():
         note_text = form.note.data
         n = Notes(note = note_text, age = age_parsing(note_text))
@@ -21,3 +21,9 @@ def NoteInput():
 @app.route('/results')
 def results():
     return render_template('results.html', results = Notes.query.all())
+
+@app.route('/risk-calc')
+def risk_calc():
+    form = RiskForm()
+    if form.validate_on_submit():
+        return f"{form.age}"
